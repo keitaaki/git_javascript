b2c05644029f5eac4ca0e5943c5acb0bb9df88d4
keitaaki <sj860069@gmail.com>
2020-01-24 06:41:36 +0900

    完成版

--- js/main.js
+++ js/main.js
@@ -10,16 +10,19 @@ const words = [
   'deluca'
 ]
 
-let word = words[Math.floor(Math.random()* words.length)];
-let loc = 0;
-let score = 0;
-let miss = 0;
+let word ;
+let loc ;
+let score ;
+let miss ;
+const timeLimit = 3 * 1000;
+let startTime;
+let isPlaying = false;
+let accuracy;
 
 const target = document.getElementById('target');
 const scoreLabel = document.getElementById('score');
 const missLabel = document.getElementById('miss');
-
-
+const timerlabel = document.getElementById('timer');
 
 function updateTarget(){
   let placeholder = '';
@@ -29,11 +32,58 @@ function updateTarget(){
   target.textContent = placeholder + word.substring(loc);
 }
 
+function updateTimer(){
+  const timeleft = startTime + timeLimit - Date.now();
+  timerlabel.textContent = (timeleft / 1000).toFixed(2);
+
+  const timeoutId =  setTimeout(() => {
+    updateTimer();
+  }, 10);
+  
+  if(timeleft < 0){
+    isPlaying = false;
+    clearTimeout(timeoutId);
+    timerlabel.textContent ="0.00";
+    setTimeout(() => {
+      showResult();
+    }, 100);
+
+    target.textContent = 'click to replay';
+  }
+}
+
+function showResult(){
+  if(score + miss === 0){
+    accuracy = 0;
+  } else {
+    accuracy = score / (score + miss) * 100;
+  }
+  alert(`${score} letters, ${miss} misses, ${accuracy.toFixed(2)}% accuracy!`);
+}
+
 window.addEventListener('click', () => {
+  if(isPlaying === true){
+    return;
+  }
+  isPlaying = true;
+
+  loc = 0;
+  score = 0;
+  miss = 0;
+  scoreLabel.textContent = score;
+  missLabel.textContent = miss;
+  word = words[Math.floor(Math.random() * words.length)];
+
   target.textContent = word;
+  startTime = Date.now();
+  updateTimer();
+  
 })
 
 window.addEventListener('keydown', (e) => {
+  if(isPlaying !== true){
+    return;
+  }
   if(e.key === word[loc]){
     loc++;
     if(loc === word.length){
